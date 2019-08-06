9d84505b3ec8930a391e594ebbace889d50e97c2
rich <rich@nedlinux>
2019-08-06 22:18:09 +0100

    Added AvRescale functions (from https://github.com/hysios/goav)

--- avutil/avutil.go
+++ avutil/avutil.go
@@ -70,3 +70,15 @@ func AvFopenUtf8(p, m string) *File {
 func AvGetTimeBaseQ() Rational {
 	return (Rational)(C.av_get_time_base_q())
 }
+
+func AvRescale(a, b, c int64) int64 {
+	return (int64)(C.av_rescale((C.int64_t)(a), (C.int64_t)(b), (C.int64_t)(c)))
+}
+
+func AvRescaleQ(a int64, bq, cq Rational) int64 {
+	return (int64)(C.av_rescale_q((C.int64_t)(a), (C.struct_AVRational)(bq), (C.struct_AVRational)(cq)))
+}
+
+func AvRescaleRnd(a, b, c int64, rouding int) int64 {
+	return (int64)(C.av_rescale_rnd((C.int64_t)(a), (C.int64_t)(b), (C.int64_t)(c), (C.enum_AVRounding)(rouding)))
+}
